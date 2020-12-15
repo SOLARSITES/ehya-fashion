33c4c6de7d19da4268bc88bcf108736c2ed9183e
SOLARSITES <iourion@gmail.com>
2020-12-15 14:07:48 +0300

    create: added header.navbar with responsive for desktop, tablet and mobile

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 $(document).ready(function () {
+  var menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener("click", function () {
+    document.querySelector("#body").classList.toggle("body-locked");
+    document.querySelector(".menu-button").classList.toggle("menu-button--close");
+    document.querySelector(".navbar-group").classList.toggle("navbar-group--visible");
+  });
+
   var tabItem = $(".tab__item");
   var contentItem = $(".content__item");
-
   tabItem.on("click", function (event) {
     var activeContent = $(this).attr("data-target");
     tabItem.removeClass("tab__item--active");
