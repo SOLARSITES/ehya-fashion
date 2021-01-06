74150fffc60d42e6dc8ac20508e864e5da30d278
SOLARSITES <iourion@gmail.com>
2021-01-06 07:00:23 +0300

    create: added block .modal with responsive for desktop, tablet & mobile

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 $(document).ready(function () {
   var menuButton = document.querySelector(".menu-button");
   menuButton.addEventListener("click", function () {
-    document.querySelector("#body").classList.toggle("body-locked");
     document.querySelector(".menu-button").classList.toggle("menu-button--close");
     document.querySelector(".navbar-group").classList.toggle("navbar-group--visible");
+    document.querySelector(".navbar-overlay").classList.toggle("navbar-overlay--visible");
   });
 
-  $(document).click(function (e) {
+  $(document).mousedown(function (e) {
     if (
       !$(".menu-button,.navbar-group").is(e.target) &&
       $(".navbar-group").has(e.target).length === 0
     ) {
-      $("#body").removeClass("body-locked");
+      $(".navbar-overlay").removeClass("navbar-overlay--visible");
       $(".menu-button").removeClass("menu-button--close");
       $(".navbar-group").removeClass("navbar-group--visible");
     }
@@ -26,4 +26,77 @@ $(document).ready(function () {
     $(activeContent).addClass("trends-content--active");
     $(this).addClass("trends-menu__item--active");
   });
+
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+
+  function openModal() {
+    var targetModal = $(this).attr("data-href");
+    $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
+    $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
+    $(".menu-button").removeClass("menu-button--close");
+    $(".navbar-group").removeClass("navbar-group--visible");
+    $(".navbar-overlay").removeClass("navbar-overlay--visible");
+    $(this).attr("navbar__button", true);
+    $(".modal__close").focus();
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+    $("[navbar__button]").focus();
+  }
+
+  $(document).mousedown(function (e) {
+    if (
+      !$(".modal__close,.modal__dialog").is(e.target) &&
+      $(".modal__dialog").has(e.target).length === 0
+    ) {
+      closeModal(e);
+    }
+  });
+
+  $(window).keyup(function (e) {
+    if (e.keyCode == 27) {
+      closeModal(e);
+    }
+  });
+
+  //Валидация форм
+  $(".form").each(function () {
+    $(this).validate({
+      errorClass: "invalid",
+      rules: {
+        phone: {
+          required: true,
+          minlength: 18,
+        },
+      },
+      messages: {
+        name: {
+          required: "Пожалуйста, укажите Ваше полное имя!",
+          minlength: "Имя должно содержать не менее 2-х букв!",
+        },
+        phone: {
+          required: "Пожалуйста, укажите Ваш телефон!",
+          minlength: "Телефон должен быть в формате +7 (xxx) xxx-xx-xx",
+        },
+        email: {
+          required: "Пожалуйста, укажите Ваш E-mail!",
+          email: "E-mail должен быть в формате name@domain.com",
+        },
+        newsletter_email: {
+          required: "Пожалуйста, укажите Ваш E-mail!",
+          email: "E-mail должен быть в формате name@domain.com",
+        },
+      },
+    });
+  });
+
+  $(`[type="tel"]`).mask("+7 (000) 000-00-00");
 });
---
+++ send.php
@@ -0,0 +1,76 @@
+<?php
+// Файлы PHPMailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$newsletter_email = $_POST['newsletter_email'];
+$message = $_POST['message'];
+
+// Формирование письма основной формы
+$title = "Новое обращение с сайта Ehya Fashion";
+$body = "
+<h2>Новое обращение с сайта Ehya Fashion</h2>
+<b>Имя:</b> $name<br>
+<b>Телефон:</b> $phone<br>
+<b>E-mail:</b> $email<br><br>
+<b>Сообщение:</b><br>$message
+";
+
+// Формирование письма для блока NEWSLETTER
+if ($newsletter_email) {
+$title = "Подписка на новости сайта Ehya Fashion";
+$body = "
+<h2>Подписка на новости сайта Ehya Fashion</h2>
+<b>E-mail:</b> $newsletter_email<br>
+";
+}
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    // $mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username   = 'revelatio@gmail.com'; // Логин на почте
+    $mail->Password   = 'diamfadwekupztma'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('revelatio@gmail.com', 'Revelatio'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('glo.mail@protonmail.com');
+
+// Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;
+
+// Проверяем факт отправки сообщения
+if ($mail->send()) {$result = "success";}
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата отправки из основной формы
+header('Location: thankyou.html');
+
+// Отображение результата отправки из блока NEWSLETTER
+if ($newsletter_email) {
+header('Location: subscribed.html');
+}
+
+// Отображение результата для случая дебага
+// echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
